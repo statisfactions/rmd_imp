39d629b173528da0b0e3a97c040b1867b1d050ab
fan ouyang <ouyan064@umn.edu>
2015-06-02 16:04:43 -0500

    add get_question

---
+++ helpers.R
@@ -0,0 +1,7 @@
+# This function returns detailed question information of the questionid
+get_question=function(questionid){
+stack_questions(questionid, filter = "!0X*T(a-7Fx5pnd2xvT*AzfJz2")
+}
+
+
+
