5e078e0bd50d3f43c6ba64bf9ac96648f46656ae
fan ouyang <ouyan064@umn.edu>
2015-06-02 16:08:21 -0500

    add get_answers

--- helpers.R
+++ helpers.R
@@ -1,7 +1,10 @@
 # This function returns detailed question information of the questionid
-get_question=function(questionid){
-stack_questions(questionid, filter = "!0X*T(a-7Fx5pnd2xvT*AzfJz2")
+get_question = function(questionid){
+  stack_questions(questionid, filter = "!0X*T(a-7Fx5pnd2xvT*AzfJz2")
 }
 
-
+# This function returns detailed answer information of the questionid
+get_answers = function(questionid){
+  stack_questions(questionid, special = "answers", filter = "!0X*T(a-7Fx5pnd2xvT*AzfJz2")  
+} 
 
