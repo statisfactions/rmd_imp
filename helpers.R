143ff45c1a79aff5107a8056d6976be5b2bf6171
Ethan Brown <brow3821@umn.edu>
2015-06-02 16:23:04 -0500

    add requirements; get_questionid; get_sitename

--- helpers.R
+++ helpers.R
@@ -1,3 +1,26 @@
+require(stringr)
+require(stackr)
+require(magrittr)
+
+# Returns question id, given question URL
+get_questionid = function(question_url) {
+  
+  id = str_extract(question_url, "/[0-9]+/") %>% 
+    str_replace_all("/", "") %>%
+    as.numeric
+  
+  id
+}
+
+# Returns site name, given question URL
+get_sitename = function(question_url) {
+  str_end = str_locate(question_url, ".com")[1] - 1
+  str_start = 8
+  sitename = str_sub(question_url, str_start, str_end)
+  
+  sitename
+}
+
 # This function returns detailed question information of the questionid
 get_question = function(questionid){
   stack_questions(questionid, filter = "!0X*T(a-7Fx5pnd2xvT*AzfJz2")
@@ -7,4 +30,5 @@ get_question = function(questionid){
 get_answers = function(questionid){
   stack_questions(questionid, special = "answers", filter = "!0X*T(a-7Fx5pnd2xvT*AzfJz2")  
 } 
+
 
